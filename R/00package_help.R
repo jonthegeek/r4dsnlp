8e5b82195b4781585592395c4bcca2ccf97a266d
Jon Harmon <jonthegeek@gmail.com>
2020-01-23 10:53:27 -0600

    Initial package setup.

---
+++ R/00package_help.R
@@ -0,0 +1,11 @@
+#' r4dsnlp: Text Analysis of the R4DS Online Learning Community
+#'
+#' The R4DS Online Learning Community is a community of R learners at all skill
+#' levels working together to improve their skills. The community features a
+#' Slack workspace, on which learners ask questions and mentors help them find
+#' answers. Here we use the BERT NLP model from Google to analyze those
+#' questions and answers.
+#'
+#' @docType package
+#' @name r4dsnlp-package
+NULL
